0781c49335a9e44ad9ac262a6d4d2934eb65121a
Pratyush Singh <pratyushsingh736@gmail.com>
2023-11-20 20:13:22 +0530

    Landing page

---
+++ Coming soon page/script.js
@@ -0,0 +1,24 @@
+let countDownTime = new Date("Dec 19, 2023 00:00:00").getTime();
+
+let x = setInterval(function() {
+    let now = new Date().getTime();
+    let distance = countDownTime - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById('days').innerHTML = days;
+    document.getElementById('hours').innerHTML = hours;
+    document.getElementById('minutes').innerHTML = minutes;
+    document.getElementById('seconds').innerHTML = seconds;
+
+    if(distance < 0) {
+        clearInterval(x);
+        document.getElementById('days').innerHTML = "00";
+        document.getElementById('hours').innerHTML = "00";
+        document.getElementById('minutes').innerHTML = "00";
+        document.getElementById('seconds').innerHTML = "00";
+    }
+}, 1000);
