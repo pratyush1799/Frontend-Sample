6fdab6d5e7ec850893079ad703ea3d48dbd7d68b
Pratyush Singh <pratyushsingh736@gmail.com>
2023-11-27 23:24:50 +0530

    Digital Clock

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -8,4 +8,4 @@ setInterval(() => {
     hrs.innerHTML = (time.getHours() < 10 ? "0":"") + time.getHours();
     min.innerHTML = (time.getMinutes() < 10 ? "0":"") + time.getMinutes();
     sec.innerHTML = (time.getSeconds() < 10 ? "0":"") + time.getSeconds();
-})
+}, 1000)
